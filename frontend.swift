22f53c073c1f34043396335e777d2bc840d27fdd
Abdullah25-Jan <mabdullahshuaib@icloud.com>
2025-03-10 16:56:53 -0400

    Update frontend.swift

--- frontend.swift
+++ frontend.swift
@@ -1,19 +1,17 @@
 // Import all necessary modules
-import FoundationNetworking
 import Foundation
-
-// Define important constants
-let url = URL(string: "http://192.168.50.235:8000/")! // This URL is where the backend server is located. Must be on same network to use this for now 
-let boundary = "monkeysareawesomesauce" // Boundary string to separate each part of multipart request
-let newline = "\r\n".data(using: .utf8)
 
 // This function takes in a path to an image as a string, converts it into a multipart-form request and prints labeled image output to the console
 func sendImage(file_path: String) {
-
+    
+    // Define important constants
+    let url = URL(string: "http://172.17.40.176:8000/")! // This URL is where the backend server is located. Must be on same network to use this for now
+    let boundary = "monkeysareawesomesauce" // Boundary string to separate each part of multipart request
+    let newline = "\r\n".data(using: .utf8)
 
     let file = URL(fileURLWithPath: file_path)
 
-    // Handle possible errors with the file 
+    // Handle possible errors with the file
     if let file_binary = try?Data(contentsOf:file) {
 
         var http_request = URLRequest(url:url)
@@ -37,21 +35,22 @@ func sendImage(file_path: String) {
         request_body.append(newline!)
         http_request.httpBody = request_body
 
+
         // Set up the POST request
         let task = URLSession.shared.dataTask(with: http_request) {(data,response,error) in
-	
-	        // Handle potential errors
-	        if let error = error {
-		        print("Error: \(error.localizedDescription)")
-		        return
-	        }
+    
+            // Handle potential errors
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+                return
+            }
 
-	        // Output labeled image response from backend server
-	        if let data = data {
-		        let responseString = String(data: data, encoding: .utf8)
-		        print("Response: \(responseString ?? "")")
-		        return
-	        }
+            // Output labeled image response from backend server
+            if let data = data {
+                let responseString = String(data: data, encoding: .utf8)
+                print("Response: \(responseString ?? "")")
+                return
+            }
         }
 
         // Actually send the request
@@ -64,5 +63,9 @@ func sendImage(file_path: String) {
 
 
 // Call the function and loop
-sendImage(file_path: "/home/avir/swift-projects/cinnamon.jpg")
-RunLoop.main.run()
+//sendImage(file_path: "/Users/muhammadabdullahshuaib/Downloads/kernel.jpg")
+//RunLoop.main.run()
+
+func world(){
+    print("Hello World")
+}
