52afc072e88a0db485540ec5617835addc37524c
aviaraina <90989129+kiwimonkey4@users.noreply.github.com>
2025-03-24 14:41:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,27 +6,23 @@ import os
 import dotenv
 import base64
 
-# Instantiate 
+# Instantiate Flask app 
 app = Flask(__name__) 
-
-# Enable cross-origin resource sharing -> Important for info sharing w/ iPhone frontend
-#CORS(app)
 
 # Load sensitive variables from .env file and set up OpenAI client in program
 dotenv.load_dotenv()
 client = openai.OpenAI(api_key = os.getenv("gpt_key"))
 gpt_model = "gpt-4o-mini" # WARNING WARNING WARNING -> DO NOT CHANGE THIS MODEL
 
-# Function takes in image and converts it through binary -> base64 -> utf-8 to be processed by API 
 def image_converter(img):
+    '''Function takes in an image as input and converts it to binary -> base64 and then returns utf-8 for API processing ''
     return base64.b64encode(img.read()).decode("utf-8")
 
 # Create route for URL and ensure route only handles POST request
 @app.route('/', methods = ['POST'])
 
-# Function sends labeled image back to client
 def home():
-
+    '''This functions sends a labeled image back to the client'''
     # Access image file in HTTP request and encode to send over HTTP 
     file = request.files["file"]
     converted_image = image_converter(file) # Convert image into format usable to send via HTTP 
@@ -63,10 +59,11 @@ def home():
             ],
             max_tokens = 15 # Sets a maximum value on length of response 
         )
-    
+
+    # Send the image label to the client
     label = response.choices[0].message.content
     return label
 
-# Starts the Flask application 
+# Start the Flask application 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0",port = 8000, debug = True) # Set flask to listen for input on port 8000 and ensure requests can be accepted from any device on a given network
